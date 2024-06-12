6f8ef3ed91673bbc6c5f36e03972efc81e225a29
Justine Tobithe Doloiras <justine@nxt.work>
2024-06-12 15:01:03 +0800

    Full updates

--- src/app/(protected)/dashboard/layout.tsx
+++ src/app/(protected)/dashboard/layout.tsx
@@ -1,19 +1,33 @@
 import AuthOptions from '@/lib/AuthOptions';
 import { getServerSession } from 'next-auth';
-import React, { FC } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 
-const Layout: FC<{
-  admin: React.ReactNode;
-  user: React.ReactNode;
-}> = async ({ admin }) => {
-  const session = await getServerSession(AuthOptions);
+interface LayoutProps {
+  admin: ReactNode;
+  user: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ admin, user }) => {
+  const [session, setSession] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      try {
+        const sessionData = await getServerSession(AuthOptions);
+        setSession(sessionData);
+      } catch (error) {
+        console.error('Error fetching session:', error);
+      }
+    };
+    fetchSession();
+  }, []);
 
   const renderContent = () => {
-    switch (session?.user.role) {
+    switch (session?.user?.role) {
       case 'admin':
         return admin;
       case 'user':
-        return admin;
+        return user;
       default:
         return null;
     }
