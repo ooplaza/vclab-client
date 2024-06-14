de27ea5612aba1ba06fea8a9a4b5854190d5d772
Justine Tobithe Doloiras <justine@nxt.work>
2024-06-15 04:57:58 +0800

    Recent updates

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,45 +1,31 @@
 import React from 'react';
-import { GetServerSideProps } from 'next';
 import { UserResponse } from '@/types/User';
 import { api } from '@/lib/api';
 import ProfileForm from './components/ProfileForm';
 
-interface ProfilePageProps {
-  user: UserResponse['data'];
-}
-
-const ProfilePage: React.FC<ProfilePageProps> = ({ user }) => {
-  return (
-    <>
-      <h1 className='text-[2rem] font-bold'>Edit Profile</h1>
-      <ProfileForm user={user} />
-    </>
-  );
-};
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
+const page = async () => {
   try {
-    const response = await api.get<UserResponse>(`/api/auth/profile`, {
-      headers: {
-        cookie: context.req.headers.cookie || '',
-      },
-    });
+
+    const response = await api.get<UserResponse>(`/api/auth/profile`);
 
     const userData = response.data.data;
 
-    return {
-      props: {
-        user: userData,
-      },
-    };
+    return (
+      <>
+        <h1 className='text-[2rem] font-bold'>Edit Profile</h1>
+        <ProfileForm user={userData} />
+      </>
+    );
   } catch (error) {
     console.error('Error fetching profile:', error);
-    return {
-      props: {
-        user: null,
-      },
-    };
+    return (
+      <>
+        <h1 className='text-[2rem] font-bold'>Edit Profile</h1>
+        <p>Error fetching profile. Please try again.</p>
+      </>
+    );
   }
 };
 
-export default ProfilePage;
+
+export default page;
